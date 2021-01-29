20400f5cdc91459fba85278b3af2b4190084c2c1
Ionos Cloud SDK CI Workflow <sdk@cloud.ionos.com>
2021-01-29 18:13:08 +0000

    CI: auto-generated version v5.0.0

--- lib/ionoscloud/api_client.rb
+++ lib/ionoscloud/api_client.rb
@@ -340,7 +340,7 @@ module Ionoscloud
     def update_params_for_auth!(header_params, query_params, auth_names)
       Array(auth_names).each do |auth_name|
         auth_setting = @config.auth_settings[auth_name]
-        next unless auth_setting
+        next unless auth_setting[:value]
         case auth_setting[:in]
         when 'header' then header_params[auth_setting[:key]] = auth_setting[:value]
         when 'query'  then query_params[auth_setting[:key]] = auth_setting[:value]
